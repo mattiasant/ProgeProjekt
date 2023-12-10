aa849dc8970f92ece14cef636b31b7dc534ac279
Mattias Antsov <mattias.antsov@gmail.com>
2023-12-10 18:23:45 +0200

    Bug fixes, added compatibility settings

--- gameinconsole.py
+++ gameinconsole.py
@@ -2,46 +2,56 @@ import random
 import sys
 import time
 import pygame
+import emojis
+import setup
 from dodge import väldi
 from mccree import gunslinger
 
 # Initialize Pygame
-pygame.init()
 
 # Constants
-WIDTH, HEIGHT = 1500, 600
+# WIDTH, HEIGHT = 900, 600
 BACKGROUND_COLOR = (0, 0, 0)
 TEXT_COLOR = (255, 255, 255)
 FONT_SIZE = 16
 FONT_NAME = 'SpaceMono-Regular.ttf'
 TERMINAL_MARGIN = 16
 
+# TKINTER
+
+
 introjooks = True
 kysievent = False
 olukord = ''
 tmain = None
-hasrun=False
+koodrun = False
+hasrun = False
 
 niki = pygame.mixer.Sound('RomanHolidayNickiMinaj.mp3')
 kaktus = pygame.mixer.Sound('MarioFall.mp3')
 bandito = pygame.mixer.Sound('USAAnthemModified.mp3')
 
-pygame.mixer.Sound.set_volume(niki, 0.1) #0.4
-pygame.mixer.Sound.set_volume(kaktus, 0.1) #0.8
-pygame.mixer.Sound.set_volume(bandito, 0.1) #0.4
+pygame.mixer.Sound.set_volume(niki, 0.1)  # 0.4
+pygame.mixer.Sound.set_volume(kaktus, 0.1)  # 0.8
+pygame.mixer.Sound.set_volume(bandito, 0.1)  # 0.4
 
-# Terminal properties
-TERMINAL_ROWS = HEIGHT // FONT_SIZE
-TERMINAL_COLS = WIDTH // (FONT_SIZE // 2)
-CURSOR_BLINK_INTERVAL = 0.5  # Cursor blink interval in seconds
-CURSOR_WIDTH = 2  # Width of the cursor block
 
 # Create the Pygame window
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Unix-like Terminal Emulator")
+def createscreen(väärtus, laius, korgus):
+    global screen
+    print(WIDTH, HEIGHT)
+    if väärtus:
+        screen = pygame.display.set_mode((laius, korgus), pygame.FULLSCREEN, pygame.SCALED)
+    else:
+        screen = pygame.display.set_mode((laius, korgus), pygame.RESIZABLE)
+    pygame.display.set_caption(
+        emojis.encode('veri e:b:ik geim @alo palun anna meile max punktid :pleading_face::point_right::point_left:'))
+    pygame.display.set_icon(pygame.image.load("removingthepolishwithchemicals.png"))
+
 
 # Define a font
 font = pygame.font.Font(FONT_NAME, FONT_SIZE)
+fontsuur = pygame.font.Font(FONT_NAME, 72)
 
 # Terminal content
 terminal_lines = []
@@ -68,9 +78,10 @@ narrator_lines = [
     'Karavan sõitis teist mõõda ja sealt hüppas välja el bandito kellel oli pew pew ja ta tahab sind siit ilmast pagendada'
 ]
 # Main loop
-running = True
+running = False
 input_lines = []  # Store individual input lines
 input_text = ""
+kood = ""
 difficulty = "default"
 
 
@@ -89,26 +100,58 @@ def ol1():
             # all_lines.append("Uurides lendavat kotkast taevas kõndisite kaktusele otsa ja kaotasite 1 elu.")
 
 
+if setup.setupdone:
+
+    rez = setup.suurus
+    rez = rez.split(' ')
+    WIDTH = (int(rez[0]))
+    HEIGHT = (int(rez[1]))
+    try:
+        scale = float(setup.scale)
+    except:
+        pass
+    if setup.var1.get() == 1:
+        createscreen(True, WIDTH, HEIGHT)
+        fs = True
+    else:
+        createscreen(False, WIDTH / scale, HEIGHT / scale)
+        fs = False
+
+    # Terminal properties
+    TERMINAL_ROWS = HEIGHT // FONT_SIZE
+    TERMINAL_COLS = WIDTH // (FONT_SIZE // 2)
+    CURSOR_BLINK_INTERVAL = 0.5  # Cursor blink interval in seconds
+    CURSOR_WIDTH = 2  # Width of the cursor block
+
+    running = True
+
 while running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            pygame.quit()
+            quit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+                pygame.quit()
+                quit()
             elif event.key == pygame.K_RETURN:
                 final_lines.append((input_text, False))
-                y += FONT_SIZE + 5
+                # y += FONT_SIZE + 5
+
+                if not koodrun:
+                    kood = ''
 
                 # Handle difficulty selection
                 if difficulty == 'default':
                     if input_text == 'kerge':
                         difficulty = 'kerge'
-                        inventory['Inventory']=''
-                        inventory['elud']=9
-                        inventory['Louis Vuitton LimitedEdition Gucci collab handbag']=1
-                        inventory['Lasud']=6
+                        inventory['Inventory'] = ''
+                        inventory['elud'] = 9
+                        inventory['Louis Vuitton LimitedEdition Gucci collab handbag'] = 1
+                        inventory['Lasud'] = 6
                     elif input_text == 'mõõdukas':
                         difficulty = 'mõõdukas'
                         inventory['Inventory'] = ''
@@ -124,24 +167,44 @@ while running:
                     if difficulty != 'default':
                         kysievent = True
                         kysieventolukord = True
-                        y += FONT_SIZE + 5
+                        # y += FONT_SIZE + 5
+                        hasrun = True
 
-                hasrun=True
                 if input_text == 'exit' or input_text == 'logout':
                     running = False
                 input_text = ""
             elif event.key == pygame.K_BACKSPACE:
                 input_text = input_text[:-1]
+                if not koodrun:
+                    kood = kood[:-1]
+            elif not koodrun and event.key == pygame.K_UP:
+                kood += 'u'
+            elif not koodrun and event.key == pygame.K_DOWN:
+                kood += 'd'
+            elif not koodrun and event.key == pygame.K_LEFT:
+                kood += 'l'
+            elif not koodrun and event.key == pygame.K_RIGHT:
+                kood += 'r'
             else:
                 input_text += event.unicode
+                if not koodrun and event.unicode == 'b':
+                    kood += event.unicode
+                elif not koodrun and event.unicode == 'a':
+                    kood += event.unicode
 
-    if hasrun==True and inventory['elud'] <= 0:
-        running=False
+    if hasrun and inventory['elud'] <= 0:
+        etime = time.time()
+        if time.time() - etime > 5:
+            running = False
+    if not koodrun and hasrun is True and input_text == 'ba':
+        if kood == 'uuddlrlrba':
+            koodrun = True
+            kood = None
+            inventory['elud'] = inventory['elud'] + 30
 
     # Clear the screen
     screen.fill(BACKGROUND_COLOR)
     y = TERMINAL_MARGIN
-
     if kysievent:
         t1 = time.time()
         t0 = time.time()
@@ -155,15 +218,15 @@ while running:
             kysievent = False
             final_lines.append((narrator_lines[0], True))
         rand0 = random.randint(1, 2)
-        if t0 != None and time.time() - t0 >= 4 and rand0 == 1:
+        if t0 is not None and time.time() - t0 >= 4 and rand0 == 1:
             final_lines.append(('Veider, põõsas ei ole kedagi. Aga ei noh väike skiso käibki asja juurde', True))
             t0 = None
             tmain = time.time()
             olukord = -1
-        elif t0 != None and time.time() - t0 >= 4 and rand0 == 2:
+        elif t0 is not None and time.time() - t0 >= 4 and rand0 == 2:
             final_lines.append(('Põõsast hüppas välja Nicki Minaj ja viskas ühe anaconda sulle näkku. -1 HP', True))
             pygame.mixer.Sound.play(niki, 0, 0, 0)
-            inventory['elud'] = inventory['elud'] -1
+            inventory['elud'] = inventory['elud'] - 1
             t0 = None
             olukord = -1
             tmain = time.time() + 4
@@ -176,12 +239,12 @@ while running:
         if vaartus == 'pos':
             final_lines.append(
                 (
-                "Uurides lendavat kotkast taevas oleksite peaaegu kaktusele otsa kõndinud, aga õnneks teil olid kiired jalad",
-                True))
+                    "Uurides lendavat kotkast taevas oleksite peaaegu kaktusele otsa kõndinud, aga õnneks teil olid kiired jalad",
+                    True))
         elif vaartus == 'neg':
             final_lines.append(("Uurides lendavat kotkast taevas kõndisite kaktusele otsa ja kaotasite 1 elu.", True))
             pygame.mixer.Sound.play(kaktus)
-            inventory['elud'] = inventory['elud']-1
+            inventory['elud'] = inventory['elud'] - 1
         # y += FONT_SIZE + 5
         tmain = time.time() + 3
         # olukord = -1
@@ -195,7 +258,7 @@ while running:
             if aeg <= 500:
                 final_lines.append(('Lasite el banditot, kes sai surma. -9999999999999999 social credit', True))
                 try:
-                    inventory['Social Credit'] = inventory['Social Credit'] -9999999999999999
+                    inventory['Social Credit'] = inventory['Social Credit'] - 9999999999999999
                 except:
                     inventory['Social Credit'] = -9999999999999999
                 pygame.mixer.Sound.play(bandito, 0, 0, 0)
@@ -209,12 +272,13 @@ while running:
 
                 except:
                     inventory['Emotional Damage'] = 10
-                inventory['Louis Vuitton LimitedEdition Gucci collab handbag'] = inventory['Louis Vuitton LimitedEdition Gucci collab handbag'] - 1
-                inventory['elud'] = inventory['elud'] -2
+                inventory['Louis Vuitton LimitedEdition Gucci collab handbag'] = inventory[
+                                                                                     'Louis Vuitton LimitedEdition Gucci collab handbag'] - 1
+                inventory['elud'] = inventory['elud'] - 2
                 tmain = time.time()
             olukord = -1
 
-    if olukord == -1 and tmain != None and time.time() - tmain >= 5:
+    if olukord == -1 and tmain is not None and time.time() - tmain >= 5:
         kysievent = True
         tmain = None
 
@@ -222,37 +286,46 @@ while running:
     all_lines = terminal_lines + input_lines + event_lines
 
     # Display terminal content
-    luser_surface = font.render('C:\\Users\Glean>', True, TEXT_COLOR)
-    screen.blit(luser_surface, (16, HEIGHT - 16 - FONT_SIZE))
+    luser_surface = font.render("C:\\Users\Glean>", True, TEXT_COLOR)
+    if fs:
+        screen.blit(luser_surface, (16, HEIGHT - 16 - FONT_SIZE - 400))
+    else:
+        screen.blit(luser_surface, (16, HEIGHT - 16 - FONT_SIZE - 400))
 
     # Blit terminal content
-    invy=400
+    invy = 400
     for item in inventory:
-
         inv_surface = font.render(f'{item}: {inventory[item]}', True, TEXT_COLOR)
-        screen.blit(inv_surface,(800,invy))
-        invy+=5+FONT_SIZE
+        screen.blit(inv_surface, (800, invy))
+        invy += 5 + FONT_SIZE
 
     for line in final_lines:
-        if line[1] == True:
+        if line[-1]:
             user_surface = font.render('C:\\Users\Alexa>', True, TEXT_COLOR)
+            screen.blit(user_surface, (TERMINAL_MARGIN, y))
+        elif line[-1] is None:
+            user_surface = font.render('C:\\System', True, TEXT_COLOR)
             screen.blit(user_surface, (TERMINAL_MARGIN, y))
         else:
             user_surface = font.render('C:\\Users\Glean>', True, TEXT_COLOR)
             screen.blit(user_surface, (TERMINAL_MARGIN, y))
-        text_surface = font.render(line[0], True, TEXT_COLOR)
+        try:
+            text_surface = font.render(line[0], True, TEXT_COLOR)
+        except:
+            text_surface = font.render(str(line[0:-1]), True, TEXT_COLOR)
         screen.blit(text_surface, (TERMINAL_MARGIN + 160, y))
         y += FONT_SIZE + 5
 
     # Display the current input text
     input_surface = font.render(input_text, True, TEXT_COLOR)
-    screen.blit(input_surface, (TERMINAL_MARGIN + 160, HEIGHT - 16 - FONT_SIZE))
-
+    if fs:
+        screen.blit(input_surface, (TERMINAL_MARGIN + 160, HEIGHT - 16 - FONT_SIZE - 400))
+    else:
+        screen.blit(input_surface, (TERMINAL_MARGIN + 160, HEIGHT - 16 - FONT_SIZE - 400))
     # Cursor blink
     if time.time() - cursor_last_toggle >= CURSOR_BLINK_INTERVAL:
         cursor_visible = not cursor_visible
         cursor_last_toggle = time.time()
-
     if cursor_visible:
         cursor_x = TERMINAL_MARGIN + 160 + font.size(input_text)[0]
         cursor_y = HEIGHT - TERMINAL_MARGIN - FONT_SIZE + FONT_SIZE / 3.5
@@ -264,14 +337,14 @@ while running:
 else:
     texit = time.time()
     screen.fill(BACKGROUND_COLOR)
-    while time.time()-texit<15:
+    while time.time() - texit < 15:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-        end_surface=font.render('geim over :(',True,TEXT_COLOR)
-        screen.blit(end_surface, (WIDTH/2,HEIGHT/2))
+        end_surface = fontsuur.render('geim over :(', True, TEXT_COLOR)
+        screen.blit(end_surface, (WIDTH / 2, HEIGHT / 2))
         pygame.display.flip()
 
 pygame.quit()
---
+++ setup.py
@@ -0,0 +1,70 @@
+import tkinter as tk
+from tkinter import ttk
+import pygame
+
+'''
+def show_selected():
+    selected_reso = reso_combo.get()
+    fscreen = var1.get()
+    sound = var2.get()
+
+    print(f"Valitud resolutsioon: {selected_reso}")
+    print(f"Valitud täisekraan: {fscreen}")
+    print(f'Sound is {sound}')
+'''
+
+root = tk.Tk()
+root.title("Setup")
+root.geometry("400x350")
+root.iconbitmap("icon.ico")
+setupdone = False
+
+
+def sule():
+    global setupdone
+    global suurus
+    global scale
+    scale = sisend.get()
+    setupdone = True
+    suurus = reso_combo.get()
+    root.destroy()
+    return True
+
+
+def vahe():
+    label = tk.Label(root, text='')
+    label.pack(padx=20, pady=10)
+
+
+# Dropdown for language selection
+pygame.init()
+reso = tk.Label(root, text="Resolution", padx=20, pady=5)
+reso.pack()
+valikud = pygame.display.list_modes()
+reso_combo = ttk.Combobox(root, values=valikud)
+reso_combo.pack()
+
+vahe()
+label = tk.Label(root, text='Scaling')
+label.pack()
+sisend = tk.Entry(root)
+sisend.pack()
+
+vahe()
+
+var1 = tk.IntVar()
+checkbox = tk.Checkbutton(root, text='Fullscreen', variable=var1)
+checkbox.pack()
+
+vahe()
+
+var2 = tk.IntVar(value=1)
+checkbox = tk.Checkbutton(root, text='Sound', variable=var2)
+checkbox.pack()
+
+vahe()
+
+show_button = tk.Button(root, text="Start", command=sule)
+show_button.pack()
+
+root.mainloop()
